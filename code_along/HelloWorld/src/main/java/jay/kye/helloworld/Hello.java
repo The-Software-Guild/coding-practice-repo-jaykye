b2ee917a0877b89c8f9180f04a93fa0a30f35a93
Jay Kye <jaym.kye@gmail.com>
2021-07-26 16:19:03 -0400

    Hello world netbeans.

---
+++ code_along/HelloWorld/src/main/java/jay/kye/helloworld/Hello.java
@@ -0,0 +1,13 @@
+package jay.kye.helloworld;
+
+/**
+ *
+ * @author Jay Kye
+ */
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+        System.out.println("Hello, Someone!");
+
+    }
+}
