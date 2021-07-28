828cdac1ab6911a8a1ca6d07c5c33b1b9cfe15fb
jaykye <58639631+jaykye@users.noreply.github.com>
2021-07-28 13:10:20 -0400

    Added collections section.

---
+++ collections/src/LearnCollections.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LearnCollections {
+    public static void main(String[] args) {
+        List<Integer> list1 = new ArrayList<>();
+        // List(extends collection) is interface, ArrayList is implementation.
+        Map<String, Integer> dict = new HashMap<>();
+
+    }
+}
