17b46c379c102370dc222ebe831436ff5c3648fd
jaykye <58639631+jaykye@users.noreply.github.com>
2021-08-04 14:23:02 -0400

    ClassRoster test file added... for real.

---
+++ ClassRoster/test/com/sg/classroster/dao/ClassRosterAuditDaoStubImpl.java
@@ -0,0 +1,9 @@
+package com.sg.classroster.dao;
+
+public class ClassRosterAuditDaoStubImpl implements ClassRosterAuditDao{
+    // 이거 interface 파일 알아서 잘 찾네?
+    @Override
+    public void writeAuditEntry(String entry) throws ClassRosterPersistenceException {
+        //do nothing . . .
+    }
+}
