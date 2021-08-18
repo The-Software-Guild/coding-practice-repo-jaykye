f03dd66d52cdeb40a9ac1d3b363f05a995891d35
Jay Kye <jaym.kye@gmail.com>
2021-08-18 00:35:18 -0400

    JDBC template updated.

--- ClassRoster/test/com/sg/classroster/dao/ClassRosterAuditDaoStubImpl.java
+++
@@ -1,9 +0,0 @@
-package com.sg.classroster.dao;
-
-public class ClassRosterAuditDaoStubImpl implements ClassRosterAuditDao{
-    // 이거 interface 파일 알아서 잘 찾네?
-    @Override
-    public void writeAuditEntry(String entry) throws ClassRosterPersistenceException {
-        //do nothing . . .
-    }
-}
